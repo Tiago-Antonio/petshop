32515aabd861dea2372853a50b8905a661d2310b
Tiago Carlos <tiagoantoniocarlos.btu@gmail.com>
2025-05-06 12:43:29 -0300

    feat: add 'client' table

---
+++ database/seeders/ClientsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Client;
+
+class ClientsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Client::create([
+            'name' => 'Camila Silva',
+            'email' => 'CamilaS@example.com',
+            'phone' => '9873124321',
+            'address' => 'Avenue A, 111',
+        ]); 
+
+        Client::create([
+            'name' => 'Jane Smith',
+            'email' => 'jane@example.com',
+            'phone' => '987654321',
+            'address' => 'Avenue B, 456',
+        ]); 
+    }
+}
