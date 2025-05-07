5979d73784c22590d135198c3ba7522789202c48
Tiago Carlos <tiagoantoniocarlos.btu@gmail.com>
2025-05-07 09:11:23 -0300

    style: max-width in modulofuncionarios view

--- resources/views/livewire/funcionarios/modulofuncionarios.blade.php
+++ resources/views/livewire/funcionarios/modulofuncionarios.blade.php
@@ -1,15 +1,19 @@
 <section class="h-screen w-screen bg-slate-100 overflow-x-hidden" x-data="{ show: false }">
     <livewire:components.header.header />
     <div class="2xl:max-h-[calc(100vh-4rem)] max-w-7xl mx-auto grid grid-cols-4 gap-4 mt-8 px-8 ">
-        <div class=" col-span-4 flex justify-between">
-            <form  class="relative w-fit" wire:submit.prevent="buscar">
-                <input type="text" wire:model.debounce.100="nome_funcionario" placeholder="Pesquisar" class="px-4 py-1 pr-10 rounded-lg border border-gray-300 focus:outline-none">
+        <div class=" col-span-4 grid grid-cols-4 gap-4 ">
+            <form  class="relative w-full col-span-1" wire:submit.prevent="buscar">
+                <input type="text" wire:model.debounce.100="nome_funcionario" placeholder="Pesquisar" class="w-full px-4 py-1 pr-10 rounded-lg border border-gray-300 focus:outline-none">
                 <button type="submit" class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500">
                     <i class="fas fa-search"></i>
                 </button>
             </form>
+            <div class=" col-span-2 flex justify-center ">
+                <button wire:click="previousPage" class="px-4 py-2 bg-gray-300 rounded-md mr-2">Anterior</button>
+                <button wire:click="nextPage" class="px-4 py-2 bg-gray-300 rounded-md">Próximo</button>
+            </div>
 
-            <div>
+            <div class=" grid place-items-end">
                 <button type="button" @click="show = true"  class=" text-white px-4 py-2 rounded-lg bg-teal-700 hover:bg-teal-600 transition-all ease-in-out duration-300">
                     <i class="fa-solid fa-user-plus"></i> Adicionar
                 </button>
@@ -77,6 +81,7 @@
                     </form>
                 </div>
             </div>
+            
             @if (session()->has('success'))
                 <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 5000)" class="fixed top-4 left-1/2 transform -translate-x-1/2 z-50 bg-green-100 border border-green-400 text-green-700 px-6 py-3 rounded-lg shadow-md transition-all duration-300">
                     {{ session('success') }}
@@ -88,6 +93,7 @@
                     {{ session('error') }}
                 </div>
             @endif
+           
         </div>
         @foreach ($funcionarios as $item)
             <div class="grid gap-4 bg-white shadow-lg rounded-md px-4 py-2 relative">
@@ -158,16 +164,13 @@
                 </div>
                 <div class=" border border-black bg-slate-200 rounded-lg px-4 py-2">
                     <ul class="max-h-[86px] overflow-auto">
-                        <li class="text-sm truncate">Nome: {{ $item['name'] }}</li>
-                        <li class="text-sm truncate">Email: {{ $item['email'] }}</li>
-                        <li class="text-sm truncate" >Telefone: {{ $item['phone'] }}</li>
+                        <li class="text-sm truncate max-w-56">Nome: {{ $item['name'] }}</li>
+                        <li class="text-sm truncate max-w-56">Email: {{ $item['email'] }}</li>
+                        <li class="text-sm truncate max-w-56" >Telefone: {{ $item['phone'] }}</li>
                     </ul>
                 </div>
             </div>
         @endforeach
-        <div class=" col-span-4 flex justify-between absolute 2xl:bottom-4 left-1/2 -translate-x-1/2">
-            <button wire:click="previousPage" class="px-4 py-2 bg-gray-300 rounded-md mr-2">Anterior</button>
-            <button wire:click="nextPage" class="px-4 py-2 bg-gray-300 rounded-md">Próximo</button>
-        </div>
+        
     </div>
 </section>
