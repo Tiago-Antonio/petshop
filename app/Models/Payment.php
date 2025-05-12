022c88aebd9c1eac33e260db8cf90b0482653d0e
Tiago Carlos <tiagoantoniocarlos.btu@gmail.com>
2025-05-12 10:53:54 -0300

    feat: create sales and payments tables and migrations

---
+++ app/Models/Payment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    protected $fillable = [
+        'sale_id',
+        'payment_method',
+        'amount_paid',          
+    ];
+}
