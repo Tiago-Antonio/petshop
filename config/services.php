27148751943228e30deace3858bc35471b520865
Tiago Carlos <tiagoantoniocarlos.btu@gmail.com>
2025-06-18 14:19:16 -0300

    fix: path oath github

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/github/callback',
+        'redirect' => 'https://tacprojetos.online/auth/github/callback',
     ],
 
     'google' => [
